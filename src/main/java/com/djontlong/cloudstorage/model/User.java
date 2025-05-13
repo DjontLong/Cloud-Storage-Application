8d55679212e1e5f25d5d37c62dc83e06592159c2
DjontLong <web.steamflower@gmail.com>
2025-05-13 21:36:03 +0700

    Создал сущность User

---
+++ src/main/java/com/djontlong/cloudstorage/model/User.java
@@ -0,0 +1,66 @@
+package com.djontlong.cloudstorage.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Setter
+@Entity
+@Table(name = "users")
+public class User implements UserDetails {
+
+    // Геттеры и сеттеры
+    @Getter
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    // Реализация методов UserDetails
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // Пока у нас нет ролей, возвращаем пустой список
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
